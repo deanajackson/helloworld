2d84a4b84bc18110ee5c63aae1a07a0b23aa32b7
Deana <demaisja@gmail.com>
2020-02-03 19:06:04 -0800

    pront date

--- src/com/helloworld/HelloWorld.java
+++ src/com/helloworld/HelloWorld.java
@@ -1,7 +1,12 @@
 package com.helloworld;
+import java.util.Date;
 
+/**
+ * Created by Deana Jackson 2/3/2020
+ */
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println(new Date());
     }
 }
